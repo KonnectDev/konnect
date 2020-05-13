9c398f6a94ba8bb75307e12162a36837e361d58e
Borre van den Engel <43012105+DeAardbolMan@users.noreply.github.com>
2020-05-13 11:20:54 +0200

    merge react with laravel

---
+++ laravel_setup.md
@@ -0,0 +1,3 @@
+## Download Laragon
+https://laragon.org/download.html
+
---
+++ src/views/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import API from "../utils/API"
+
+
+
+function Homepage() {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default Homepage;
