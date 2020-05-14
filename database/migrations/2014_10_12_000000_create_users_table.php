cf89c35f4cd05a01f597b9380404c59fceb0d43a
Borre van den Engel <43012105+DeAardbolMan@users.noreply.github.com>
2020-05-14 10:17:53 +0200

    create guild & guild member

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('password');
+            $table->integer('koins');
+            $table->integer('phonenumber');
+            $table->integer('contrycode');
+            $table->longText('bio');
+            $table->date('birthdate');
             $table->rememberToken();
             $table->timestamps();
         });
