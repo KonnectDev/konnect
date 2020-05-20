7aa399cd30f0ff3bdf1ed25510767d46abec138e
Borre van den Engel <43012105+DeAardbolMan@users.noreply.github.com>
2020-05-20 16:40:01 +0200

    update git

--- updateGit.php
+++ updateGit.php
@@ -15,6 +15,6 @@ chdir(__DIR__);
     echo shell_exec('composer install');
     echo shell_exec('composer update');
     echo shell_exec('npm install');
-    //echo shell_exec('npm run build --modern');
+    echo shell_exec('react-scripts build');
 
 
