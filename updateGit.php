80f89f1569afeaf5e0034fd54705fc584ddf9ef2
Borre van den Engel <43012105+DeAardbolMan@users.noreply.github.com>
2020-05-27 09:22:58 +0200

    update updateGit.php

--- updateGit.php
+++ updateGit.php
@@ -5,13 +5,13 @@
 
     chdir('/opt/bitnami/apache2/htdocs/');
     echo 'GIT fetch' . PHP_EOL;
-
-    echo shell_exec('git fetch --all');
-    echo shell_exec('git reset --hard master');
-    echo shell_exec('git pull');
-    echo shell_exec('git checkout master');
-    echo shell_exec('sudo chmod -R 777 storage/');
-    echo shell_exec('php artisan migrate');
+    echo shell_exec('cd /opt/bitnami/apache2/htdocs/') . PHP_EOL;
+    echo shell_exec('git fetch --all') . PHP_EOL;
+    echo shell_exec('git reset --hard master') . PHP_EOL;
+    echo shell_exec('git pull') . PHP_EOL;
+    echo shell_exec('git checkout master') . PHP_EOL;
+    echo shell_exec('sudo chmod -R 777 storage/') . PHP_EOL;
+    echo shell_exec('php artisan migrate') . PHP_EOL;
 
 //    echo 'Composer & NPM' . PHP_EOL;
 //    echo shell_exec('composer install --optimize-autoloader --no-dev');
@@ -19,4 +19,6 @@
 //    echo shell_exec('npm install');
 //    echo shell_exec('npm run-script build');
 //
+
+    echo 'done' . PHP_EOL;
 
