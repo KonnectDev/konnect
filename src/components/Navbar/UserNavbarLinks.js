0a4494a99a500470cdba88e04ae7a8de4b35ac1f
Sjoerd van Dorp <sjoerdvdorp7@gmail.com>
2020-06-11 14:54:21 +0200

    zelfgemaakte navbar is af pog

--- src/components/Navbar/UserNavbarLinks.js
+++ src/components/Navbar/UserNavbarLinks.js
@@ -1,10 +1,5 @@
 import React, {Component} from "react";
 import {NavLink} from 'react-router-dom';
-import {NavItem, Nav, NavDropdown, DropdownItem} from "react-bootstrap";
-import HomeIcon from '@material-ui/icons/Home';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import SettingsIcon from '@material-ui/icons/Settings';
-import ChatIcon from '@material-ui/icons/Chat';
 import {Icon} from 'react-icons-kit';
 import {home} from 'react-icons-kit/fa/home';
 import {trophy} from 'react-icons-kit/fa/trophy';
@@ -16,6 +11,7 @@ import {bell} from 'react-icons-kit/fa/bell';
 import profile from '../../assets/img/face-3.jpg';
 import {plus} from 'react-icons-kit/iconic/plus'
 import "../../assets/css/Navbar.scss";
+import koin from '../../assets/img/Konnect_koin.svg';
 
 
 class UserNavbarLinks extends Component {
@@ -23,29 +19,31 @@ class UserNavbarLinks extends Component {
         const username = localStorage.getItem("username");
         return (
 
-            <nav className="navbar navbar-inverse" style={{width: '100%'}}>
+            <nav className="navbar navbar-inverse" style={{width: '100%', padding: "0"}}>
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <ul className="nav navbar-nav">
-                            <li><NavLink to="/Dashboard/feed" style={{color: '#dcddde'}}><Icon size={30}
+                            <li><NavLink to="/Dashboard/feed" style={{color: '#dcddde'}}><Icon size={26}
                                                                                                icon={home}/></NavLink>
                             </li>
-                            <li><NavLink to="/Dashboard/leaderboard" style={{color: '#dcddde'}}><Icon size={30}
+                            <li><NavLink to="/Dashboard/leaderboard" style={{color: '#dcddde'}}><Icon size={26}
                                                                                                       icon={trophy}/></NavLink>
                             </li>
                             <li><a href="#" style={{color: '#dcddde'}}><Icon size={30} icon={group}/></a></li>
-                            <li><NavLink to="/Dashboard/profile" style={{color: '#dcddde'}}><Icon size={30}
+                            <li><NavLink to="/Dashboard/profile" style={{color: '#dcddde'}}><Icon size={26}
                                                                                                   icon={gears}/></NavLink>
                             </li>
                             <li><a href="#" style={{color: '#dcddde'}}><Icon size={30} icon={comments}/></a></li>
                             <li><NavLink to="/" onClick={this.props.loggingout} style={{color: '#dcddde'}}><Icon
-                                size={30} icon={signOut}/></NavLink></li>
+                                size={26} icon={signOut}/></NavLink></li>
 
 
                         </ul>
                     </div>
                     <div className="navbar-header justify-content-end">
                         <ul className="nav navbar-nav ">
+                            <li style={{width: "30px", margin: "7px 10px"}}><img src={koin} style={{width: "100%"}}/>
+                            </li>
                             <li><p style={{
                                 color: '#dcddde',
                                 fontSize: "20px",
@@ -53,7 +51,7 @@ class UserNavbarLinks extends Component {
                                 margin: "0",
                                 padding: "0"
                             }}>1234</p></li>
-                            <li><a href="#" style={{color: '#dcddde', fontSize: "26px"}}><Icon size={30}
+                            <li><a href="#" style={{color: '#dcddde', fontSize: "26px"}}><Icon size={22}
                                                                                                icon={plus}/></a></li>
                             <li style={{width: "45px", margin: "0px 5px"}}><img src={profile} style={{width: "100%"}}/>
                             </li>
@@ -61,7 +59,7 @@ class UserNavbarLinks extends Component {
                                 <span style={{display: "block", fontSize: "14px", color: "#dcddde"}}>Obama Obama</span>
                                 <span style={{display: "block", fontSize: "14px", color: "#dcddde"}}>Lvl 50</span>
                             </li>
-                            <li><a href="#" style={{color: '#dcddde', fontSize: "26px"}}><Icon size={30}
+                            <li><a href="#" style={{color: '#dcddde', fontSize: "26px"}}><Icon size={22}
                                                                                                icon={bell}/></a></li>
                         </ul>
                     </div>
